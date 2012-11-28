85da0ee3b628a0f02d0fe5cc3162a59383e675cb
Edward Lau <edwardglau@gmail.com>
2012-11-28 14:38:52 -0800

    Cleaned up index.php

--- carSharing/index.php
+++ carSharing/index.php
@@ -2,12 +2,10 @@
     // Include the library
     include('simple_html_dom.php');
     
+    //scrape zimride
     $zim_url = "http://www.zimride.com";
     $folder = file_get_html($zim_url);
-    //echo $folder;
-    
-    foreach ($folder->find('div.rides ul li') as $e){
-
+    foreach ($folder->find('div.rides ul li') as $e) {
     	$image = $e->find('img',0)->src;
     	$main5 = $e->find('a',0)->childNodes(2)->innertext;
     	$main5 = str_replace('"', ' ', $main5);
@@ -18,26 +16,29 @@
         $price = $e->find('div.price_box h1',0)->innertext;
         $type = $e->find('a span.driver',0)?"driver":"passenger";
     	
-    	$arr[] = array(    
+    	$data[] = array(    
     			'link' => $link,
     			'price' => $price,
     			'image' => $image,
     			'name' => $name,
-    			'originCity' => trim($city1[0]),
+    			'originCity' => trim($city1[0])
     			'destinationCity' => trim($city2[1]),
     			'driver' => $type,
     			'date' => "today"
-    	);//print_r($arr);
-    	
+    	);
+        //print_r($data);
     }
-    $response = $arr;
-    
-    $fp = fopen('home.json', 'w');
-    fwrite($fp, json_encode($response));
-    fclose($fp);
 
+    //scrape ridejoy
+    //todo
+
+    //write to json
+    $fp = fopen('home.json', 'w');
+    fwrite($fp, json_encode($data));
+    fclose($fp);
     $folder->clear();
+
+    //write to page
     $homepage = file_get_contents('./home.html', false);
 	echo $homepage;  
-
 ?>
