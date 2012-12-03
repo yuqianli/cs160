43c994b5e92b2bde09a361fab267bb3bdb285045
arunathuluru <arunathuluru@gmail.com>
2012-12-02 23:11:22 -0800

    sort works fully. Requres some formatting
    I had to remove $ sign to insert some zeros 
    Having some problem get the dollar sign back. Will get it done ASAP

--- carSharing/sort.php
+++ carSharing/sort.php
@@ -1,25 +1,62 @@
 <?php 
-    //sorting function
-    function cmp($a, $b)
-    {
-        return strcmp($a["price"], $b["price"]);
-    }
+// Read the json  results  and parse the string into a data structure 
 
-    // Read the json  results  and parse the string into a data structure 
-    $str_data = file_get_contents("results.json"); 
-    if (!$str_data) {
-        die("Error opening json");
-    }
-    $data = json_decode($str_data,true);   
+$file_contents = file_get_contents("results.json");
 
-    //sort the data and write to json file
-    $arr = $data;
-    usort($arr,"cmp");
-    $fh = fopen("results.json", 'w');
+$fh = fopen("results.json", "w");
+$file_contents = str_replace('free','$0',$file_contents);
+$file_contents = str_replace('"$','"',$file_contents);
+
+fwrite($fh, $file_contents);
+fclose($fh);
+//"price": "$45",
+
+
+$str_data = file_get_contents("results.json"); 
+$data = json_decode($str_data,true);   
+   
+
+// Modify the value, and write the structure to a file "data_out.json" //
+
+function cmp($a, $b)
+{
+	$a["price"] = str_pad($a["price"], 10, '0', STR_PAD_LEFT);
+	$b["price"] = str_pad($b["price"], 10, '0', STR_PAD_LEFT);
+    return strcasecmp($a["price"], $b["price"]);
+}
+
+$arr = $data;
+
+usort($arr,"cmp");
+ 
+// writing the results to results.json file 
+$fh = fopen("results.json", 'w')       
+	or die("Error opening output file"); 
 	fwrite($fh, json_encode($arr,JSON_UNESCAPED_UNICODE)); 
+	
+	
+fclose($fh); 
 
-    //finish up
-    fclose($fh); 
+/*
+need to be done
+  $fh = fopen("results.json", "w");
+ // trying to put back the dollar sign
+ $file_contents = str_replace('"price": "','"price": "$',$file_contents);
+ // trying to put back the word "free" in place of $0
+ $file_contents = str_replace('"price": "$0','"price": "free',$file_contents);
+ 
+$file_contents = str_replace('"price": "0','"price": "free',$file_contents);
+
+fwrite($fh, $file_contents);
+fclose($fh);
+
+ */
     $sortpage = file_get_contents('./output.html', false);
-    echo $sortpage;   
-?>
+	echo $sortpage;   
+	
+	
+	
+	
+	
+	
+	
