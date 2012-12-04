fb1fd44285cbbefebbc9efe0ab95f3b534b6726e
Edward <edwardglau@gmail.com>
2012-12-03 17:47:56 -0800

    Fixed sort formatting.

--- carSharing/sort.php
+++ carSharing/sort.php
@@ -1,62 +1,37 @@
 <?php 
-// Read the json  results  and parse the string into a data structure 
+    //Read the json results and parse the string into a data structure 
+    $str_data = file_get_contents("results.json"); 
+    $data = json_decode($str_data,true);   
 
-$file_contents = file_get_contents("results.json");
+    //replace 'free' and '$' in prices
+    for ($i = 0; $i < count($data); $i++) {
+        $data[$i]['price'] = str_replace('free', '$0', $data[$i]['price']);
+        $data[$i]['price'] = str_replace('$', '', $data[$i]['price']);
+    }
 
-$fh = fopen("results.json", "w");
-$file_contents = str_replace('free','$0',$file_contents);
-$file_contents = str_replace('"$','"',$file_contents);
+    //sorting function, pads with 0 on left so that 0200 > 021
+    function cmp($a, $b)
+    {
+        $a["price"] = str_pad($a["price"], 10, '0', STR_PAD_LEFT);
+        $b["price"] = str_pad($b["price"], 10, '0', STR_PAD_LEFT);
+        return strcasecmp($a["price"], $b["price"]);
+    }
 
-fwrite($fh, $file_contents);
-fclose($fh);
-//"price": "$45",
+    //sort data
+    $arr = $data;
+    usort($arr,"cmp");
 
+    //put '$' back in price
+    for ($i = 0; $i < count($arr); $i++) {
+        $arr[$i]['price'] = '$' . $arr[$i]['price'];
+    }
+     
+    //write to results.json
+    $fh = fopen("results.json", 'w');
+    fwrite($fh, json_encode($arr, JSON_UNESCAPED_UNICODE)); 
 
-$str_data = file_get_contents("results.json"); 
-$data = json_decode($str_data,true);   
-   
-
-// Modify the value, and write the structure to a file "data_out.json" //
-
-function cmp($a, $b)
-{
-	$a["price"] = str_pad($a["price"], 10, '0', STR_PAD_LEFT);
-	$b["price"] = str_pad($b["price"], 10, '0', STR_PAD_LEFT);
-    return strcasecmp($a["price"], $b["price"]);
-}
-
-$arr = $data;
-
-usort($arr,"cmp");
- 
-// writing the results to results.json file 
-$fh = fopen("results.json", 'w')       
-	or die("Error opening output file"); 
-	fwrite($fh, json_encode($arr,JSON_UNESCAPED_UNICODE)); 
-	
-	
-fclose($fh); 
-
-/*
-need to be done
-  $fh = fopen("results.json", "w");
- // trying to put back the dollar sign
- $file_contents = str_replace('"price": "','"price": "$',$file_contents);
- // trying to put back the word "free" in place of $0
- $file_contents = str_replace('"price": "$0','"price": "free',$file_contents);
- 
-$file_contents = str_replace('"price": "0','"price": "free',$file_contents);
-
-fwrite($fh, $file_contents);
-fclose($fh);
-
- */
+    //finish up	
+    fclose($fh); 
     $sortpage = file_get_contents('./output.html', false);
-	echo $sortpage;   
-	
-	
-	
-	
-	
-	
-	
+    echo $sortpage;   
+?>
