5bc6bccbc2a17c707a6d0d8a151dfbf5567e5741
Edward <edwardglau@gmail.com>
2012-12-04 23:56:41 -0800

    Cleaned up results.php for better readability.

--- carSharing/results.php
+++ carSharing/results.php
@@ -70,134 +70,101 @@
     //retrieve DOM from ridejoy
     $ridejoy_url = "http://ridejoy.com/rides/search?utf8=%E2%9C%93&type=$type&origin=$originCity%2C+$originState%2C+USA&origin_latitude=$originLat&origin_longitude=$originLon&destination=$destinationCity%2C+$destinationState%2C+USA&destination_latitude=$destinationLat&destination_longitude=$destinationLon&date=";
     $ridejoy = file_get_html($ridejoy_url);
-    
     $joy = $ridejoy->find('div.rides_search_container div.date');
-    $jindex = 0;
-    $jindex2 = 0;
     
     	
-    
-    //zimride
+    /* FIX: should use the same POSTed values as ridejoy */ 
     $fromCity = str_replace(' ', '+', $origin[0]);
     $fromState = $origin[1];
     $toCity = str_replace(' ', '+', $destination[0]);
     $toState = $destination[1];
-    //$url_date = str_replace('/', '%2F', empty($_POST['date'])?$_POST['date']:"");
-    $url_date = $startDate;
+    $url_date = str_replace('/', '%2F', empty($_POST['date'])?$_POST['date']:"");
+    /* end FIX */
     
-    // Retrieve the DOM from a given URL
-    $page_url = "http://www.zimride.com/search?s=$fromCity%2C+$fromState&e=$toCity%2C+$toState&date=$url_date&s_name=$toCity%2C+$toState&s_full_text=$fromCity%2C+$fromState%2C+USA&s_error_code=&s_address=$fromCity%2C+$fromState%2C+USA&s_city=$fromCity&s_state=$fromState&s_zip=&s_country=US&s_lat=$originLat&s_lng=$originLon&s_location_key=&s_user_lat=&s_user_lng=&s_user_country=&e_name=$toCity%2C+$toState&e_full_text=$toCity%2C+$toState%2C+USA&e_error_code=&e_address=$toCity%2C+$toState%2C+USA&e_city=$toCity&e_state=$toState&e_zip=&e_country=US&e_lat=$destinationLat&e_lng=$destinationLon&e_location_key=&e_user_lat=&e_user_lng=&e_user_country=";
-    $zimride = file_get_html($page_url);
-    //echo $page_url;
-    //echo $folder;
-    
-    
-    
-    
-
-    
-    
-    
-    
-    // Find all TD tags with "align=center"
-    $i=0;
-    $cur=0;
+    //retrieve DOM from zimride
+    $zim_url = "http://www.zimride.com/search?s=$fromCity%2C+$fromState&e=$toCity%2C+$toState&date=$url_date&s_name=$toCity%2C+$toState&s_full_text=$fromCity%2C+$fromState%2C+USA&s_error_code=&s_address=$fromCity%2C+$fromState%2C+USA&s_city=$fromCity&s_state=$fromState&s_zip=&s_country=US&s_lat=$originLat&s_lng=$originLon&s_location_key=&s_user_lat=&s_user_lng=&s_user_country=&e_name=$toCity%2C+$toState&e_full_text=$toCity%2C+$toState%2C+USA&e_error_code=&e_address=$toCity%2C+$toState%2C+USA&e_city=$toCity&e_state=$toState&e_zip=&e_country=US&e_lat=$destinationLat&e_lng=$destinationLon&e_location_key=&e_user_lat=&e_user_lng=&e_user_country=";
+    $zimride = file_get_html($zim_url);
+    //find all TD tags with "align=center"
     $zim = $zimride->find('div.ride_list a') ;
-    //foreach ($zimride->find('div.ride_list a') as $e) {
+    
+    //scrape from ridejoy and zimride
+    $jindex = 0;
     $zindex=0;
-    
-    
-    $j=0;
-    
-    
-    
-    
-    
-    while(!empty($joy[$jindex]) || !empty($zim[$zindex])){
-    	
-    
-    if(!empty($joy[$jindex])){
-        //$perDay = $joy[$jindex]->find('div.rides_search_result_container'); //$joy[$jindex]
-    	$name = "";//$f->find('div.username',0)->plaintext;
-    	$link = $joy[$jindex]->childNodes(1)->childNodes($j)->find('a', 0)->href;
-    	$image = $joy[$jindex]->childNodes(1)->childNodes($j)->find('div.photo img',0)->src;
-    	$tmp = $joy[$jindex]->childNodes(1)->childNodes($j)->find('div.seat_count',0);
-    	$price = $tmp?$tmp->plaintext:"free";
-    	//$main5 = $f->find('div.inner_content span.inner',0)->innertext;
-    	$originCity = $joy[$jindex]->childNodes(1)->childNodes($j)->find('div.origin',0)->plaintext;
-    	$destinationCity = $joy[$jindex]->childNodes(1)->childNodes($j)->find('div.destination',0)->plaintext;
-    	$type = $tmp?"driver":"passenger";
-    
-    	$departure = $joy[$jindex]->find('div.date_header',0)->plaintext;
-    	//date has to be fixed, the algorithm uses $i to compare childNodes
-    	//but it has not been implemented yet.
-    
-    	$arr[] = array(    'link' => $link,
-    			'price' => $price,
-    			'image' => $image,
-    			'name' => $name,
-    			'originCity' => $originCity,
-    			'destinationCity' => $destinationCity,
-    			'driver' => $type,
-    			'date' => trim($departure)
-    	);
-    	$j++;
-    	if(!$joy[$jindex]->childNodes(1)->childNodes($j)){
-    	  $jindex++;
-    	  $j=0;
-    	}
-    	//echo "\n\t".$main5."\n";
-    }
-    
-    
-    if(!empty($zim[$zindex])){
-        $name = $zim[$zindex]->find('div.username',0);
-        //ignore last two links in page that are not people
-        if ($name) {
-        	$name = $name->plaintext;
-            $link = $zim[$zindex]->getAttribute('href');
-            $image = $zim[$zindex]->find('img',0)->src;
-            $price = $zim[$zindex]->find('div[class=price_box]',0)?$zim[$zindex]->find('div[class=price_box]',0)->childNodes(1)->childNodes(0)->plaintext:"free";
-            $main5 = $zim[$zindex]->find('div.inner_content span.inner',0)->innertext;
-            $city1 = explode("<span", $main5);
-            $city2 = explode("</span>", $main5);
-            $type = $zim[$zindex]->find('div.userpic span.driver',0)?"driver":"passenger";
-
-            $departure = $zimride->find('div.ride_list',0)->childNodes($cur)->find('span',0)->plaintext;
-            //date has to be fixed, the algorithm uses $i to compare childNodes
-            //but it has not been implemented yet. 
-
-            $arr[] = array(    'link' => $link,
-                               'price' => $price,
-                               'image' => $image,
-                               'name' => $name,
-                               'originCity' => $city1[0],
-                               'destinationCity' => $city2[1],
-                               'driver' => $type,
-                               'date' => $departure
-                         );
-            //echo "\n\t".$main5."\n";
-            $i = $i+1;
-        	if($zimride->find('div.ride_list',0)->childNodes($i)->find('em',0))
-        		$cur = $i;
+    while (!empty($joy[$jindex]) || !empty($zim[$zindex])) {
+        //scrape from ridejoy
+        $j=0;
+        if(!empty($joy[$jindex])){
+            $name = "";
+            $link = $joy[$jindex]->childNodes(1)->childNodes($j)->find('a', 0)->href;
+            $image = $joy[$jindex]->childNodes(1)->childNodes($j)->find('div.photo img',0)->src;
+            $tmp = $joy[$jindex]->childNodes(1)->childNodes($j)->find('div.seat_count',0);
+            $price = $tmp?$tmp->plaintext:"free";
+            $originCity = $joy[$jindex]->childNodes(1)->childNodes($j)->find('div.origin',0)->plaintext;
+            $destinationCity = $joy[$jindex]->childNodes(1)->childNodes($j)->find('div.destination',0)->plaintext;
+            $type = $tmp?"driver":"passenger";
+            $departure = $joy[$jindex]->find('div.date_header',0)->plaintext;
+        
+            $arr[] = array(
+                'link' => $link,
+                'price' => $price,
+                'image' => $image,
+                'name' => $name,
+                'originCity' => $originCity,
+                'destinationCity' => $destinationCity,
+                'driver' => $type,
+                'date' => trim($departure)
+            );
+            $j++;
+            if(!$joy[$jindex]->childNodes(1)->childNodes($j)) {
+                $jindex++;
+                $j=0;
+            }
         }
-        $zindex++;
+        
+        //scrape from zimride
+        $cur=0;
+        $i=0;
+        if(!empty($zim[$zindex])){
+            $name = $zim[$zindex]->find('div.username',0);
+            //ignore last two links in page that are not people
+            if ($name) {
+                $name = $name->plaintext;
+                $link = $zim[$zindex]->getAttribute('href');
+                $image = $zim[$zindex]->find('img',0)->src;
+                $price = $zim[$zindex]->find('div[class=price_box]',0)?$zim[$zindex]->find('div[class=price_box]',0)->childNodes(1)->childNodes(0)->plaintext:"free";
+                $main5 = $zim[$zindex]->find('div.inner_content span.inner',0)->innertext;
+                $city1 = explode("<span", $main5);
+                $city2 = explode("</span>", $main5);
+                $type = $zim[$zindex]->find('div.userpic span.driver',0)?"driver":"passenger";
+                $departure = $zimride->find('div.ride_list',0)->childNodes($cur)->find('span',0)->plaintext;
+
+                $arr[] = array(
+                    'link' => $link,
+                    'price' => $price,
+                    'image' => $image,
+                    'name' => $name,
+                    'originCity' => $city1[0],
+                    'destinationCity' => $city2[1],
+                    'driver' => $type,
+                    'date' => $departure
+                );
+                $i++;
+                if ($zimride->find('div.ride_list',0)->childNodes($i)->find('em',0)) {
+                    $cur = $i;
+                }
+            }
+            $zindex++;
+        }
     }
     
-    
-    
-    
-    }
-    
-    
-    //print_r($arr);
+    //write scraped data to json file
     $response = $arr;
-    
     $fp = fopen('results.json', 'w');
     fwrite($fp, json_encode($response));
     fclose($fp);
-    $ridejoy->clear();
 
+    //finish up
+    $ridejoy->clear();
     $zimride->clear();
     $homepage = file_get_contents('./results.html', false);
 	echo $homepage;       
