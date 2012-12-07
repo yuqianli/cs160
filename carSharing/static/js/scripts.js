aa92c2ad56c825f54c1ab8e28d5af4601f347b5f
Edward <edwardglau@gmail.com>
2012-12-06 16:10:40 -0800

    Added searchbar symbol validation.

---
+++ carSharing/static/js/scripts.js
@@ -0,0 +1,19 @@
+function validate() {
+    var err = [];
+    var illegal_chars = '~!@#$%^&*()_+=`:;\'"/?[]{}\\|';
+    illegal_chars = illegal_chars.split('');
+    illegal_chars.forEach(function(c) {
+        if ((document.forms['searchbars']['from'].value).indexOf(c) != -1) {
+            err.push("Illegal character '" + c + " 'in starting location!"); 
+        }
+        if ((document.forms['searchbars']['to'].value).indexOf(c) != -1) {
+            err.push("Illegal character '" + c + " 'in ending location!"); 
+        }
+    });
+    if (err.length > 0) {
+        console.log(err);
+        err = err.join('\n');
+        alert(err);
+        return false;
+    }
+}
